16917e0348fd3e4073c0429762299c3acb29a600
code_2497 <9405319+code-2497@user.noreply.gitee.com>
2021-09-18 10:14:40 +0800

    import word.segmentation, add hammingDistance

---
+++ src/main/java/Hash.java
@@ -0,0 +1,68 @@
+import org.apdplat.word.segmentation.Word;
+
+import java.math.BigInteger;
+import java.util.List;
+
+public class Hash {
+    // 生成 64 位的 SimHash
+    private int hashBitCount = 64;
+    /**
+     * 计算词列表的SimHash值，通过分词的时候已经统计了词的权重
+     * @param words 词列表
+     * @return SimHash值
+     */
+    public String simHash(List<Word> words) {
+        float[] hashBit = new float[hashBitCount];
+        words.forEach(word -> {
+            float weight = word.getWeight()==null?1:word.getWeight();
+            BigInteger hash = hash(word.getText());
+            for (int i = 0; i < hashBitCount; i++) {
+                BigInteger bitMask = new BigInteger("1").shiftLeft(i);
+                if (hash.and(bitMask).signum() != 0) {
+                    hashBit[i] += weight;
+                } else {
+                    hashBit[i] -= weight;
+                }
+            }
+        });
+
+        StringBuffer fingerprint = new StringBuffer();
+        for (int i = 0; i < hashBitCount; i++) {
+            if (hashBit[i] >= 0) {
+                fingerprint.append("1");
+            }else{
+                fingerprint.append("0");
+            }
+        }
+
+        return fingerprint.toString();
+    }
+
+    /**
+     *  Hash 算法
+     * @param word 词
+     * @return 哈希值
+     */
+    public BigInteger hash(String word) {
+        if (word == null || word.length() == 0) {
+            return new BigInteger("0");
+        }
+
+        char[] charArray = word.toCharArray();
+        BigInteger x = BigInteger.valueOf(((long) charArray[0]) << 7);
+        BigInteger m = new BigInteger("1000003");
+        BigInteger mask = new BigInteger("2").pow(hashBitCount).subtract(new BigInteger("1"));
+        long sum = 0;
+        for (char c : charArray) {
+            sum += c;
+        }
+
+        x = x.multiply(m).xor(BigInteger.valueOf(sum)).and(mask);
+        x = x.xor(new BigInteger(String.valueOf(word.length())));
+        if (x.equals(new BigInteger("-1"))) {
+            x = new BigInteger("-2");
+        }
+
+        return x;
+    }
+}
--- src/main/java/SimHashBaseOnWord.java
+++ src/main/java/SimHashBaseOnWord.java
@@ -11,9 +11,13 @@ import java.util.List;
 
 
 public class SimHashBaseOnWord extends TextSimilarity  {
+
     // 生成 64 位的 SimHash
     private int hashBitCount = 64;
+
+
     public SimHashBaseOnWord(){
+        Hash s = new Hash();
     }
 
     public SimHashBaseOnWord(int hashBitCount) {
@@ -24,66 +28,48 @@ public class SimHashBaseOnWord extends TextSimilarity  {
         return 0;
     }
 
-
     /**
-     * 计算词列表的SimHash值，通过分词的时候已经统计了词的权重
-     * @param words 词列表
-     * @return SimHash值
+     * 计算等长的SimHash值的汉明距离
+     * @param simHash1 SimHash值1
+     * @param simHash2 SimHash值2
+     * @return 汉明距离
      */
-    private String simHash(List<Word> words) {
-        float[] hashBit = new float[hashBitCount];
-        words.forEach(word -> {
-            float weight = word.getWeight()==null?1:word.getWeight();
-            BigInteger hash = hash(word.getText());
-            for (int i = 0; i < hashBitCount; i++) {
-                BigInteger bitMask = new BigInteger("1").shiftLeft(i);
-                if (hash.and(bitMask).signum() != 0) {
-                    hashBit[i] += weight;
-                } else {
-                    hashBit[i] -= weight;
-                }
-            }
-        });
+    public int hammingDistance(String simHash1, String simHash2) {
+        if (simHash1.length() != simHash2.length()) {
+            return this.hashBitCount;
+        }
 
-        StringBuffer fingerprint = new StringBuffer();
-        for (int i = 0; i < hashBitCount; i++) {
-            if (hashBit[i] >= 0) {
-                fingerprint.append("1");
-            }else{
-                fingerprint.append("0");
+        int distance = 0;
+        int len = simHash1.length();
+        for (int i = 0; i < len; i++) {
+            if (simHash1.charAt(i) != simHash2.charAt(i)) {
+                distance++;
             }
         }
 
-        return fingerprint.toString();
+        return distance;
     }
 
     /**
-     *  Hash 算法
-     * @param word 词
-     * @return 哈希值
+     * 对文本进行分词
+     * @param text
+     * @return
      */
-    private BigInteger hash(String word) {
-        if (word == null || word.length() == 0) {
-            return new BigInteger("0");
+    private List<Word> seg(String text) {
+        if(text == null){
+            return Collections.emptyList();
         }
 
-        char[] charArray = word.toCharArray();
-        BigInteger x = BigInteger.valueOf(((long) charArray[0]) << 7);
-        BigInteger m = new BigInteger("1000003");
-        BigInteger mask = new BigInteger("2").pow(hashBitCount).subtract(new BigInteger("1"));
-        long sum = 0;
-        for (char c : charArray) {
-            sum += c;
+        Segmentation segmentation  = SegmentationFactory.getSegmentation(SegmentationAlgorithm.MaxNgramScore);
+        List<Word> words = segmentation.seg(text);
+        if(filterStopWord) {
+            //停用词过滤
+            StopWord.filterStopWords(words);
         }
-
-        x = x.multiply(m).xor(BigInteger.valueOf(sum)).and(mask);
-        x = x.xor(new BigInteger(String.valueOf(word.length())));
-        if (x.equals(new BigInteger("-1"))) {
-            x = new BigInteger("-2");
-        }
-
-        return x;
+        return words;
     }
+
+
 
 
 }
